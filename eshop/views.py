0b25269e99211bcd46d2d16f5b3112a37240b39f
divyasunil <divyaindu13@gmail.com>
2024-02-22 08:26:18 +0400

    first

---
+++ eshop/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from eshop.models import Product, Category
+
+# Create your views here.
+# def index(request):
+#     return HttpResponse("Hai")
+
+def allProdCat(request, c_slug=None):
+    c_page = None
+    products = None
+    if c_slug != None:
+        c_page = get_object_or_404(Category, slug=c_slug)
+        products = Product.objects.all().filter(category=c_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+
+    return render(request, 'category.html', {'category': c_page, 'products': products, })
